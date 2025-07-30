4b5d2f57fbb3dc8c6249a1a50d3c36777260176a
yogadeepi <yogadeepika13122004@gmail.com>
2025-07-30 21:44:58 +0530

    Space.java

---
+++ Space.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.printf("%05d\n" ,a);
+        System.out.printf("%5d" ,b);
+        
+    }
+}
