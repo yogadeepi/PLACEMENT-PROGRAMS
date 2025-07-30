33ca2cf6311b70013a4a1e3b57441022adddf990
yogadeepi <yogadeepika13122004@gmail.com>
2025-07-30 21:19:40 +0530

    OddNumber.java

---
+++ OddNumber.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+            if(i%2==1)
+                System.out.print(i+" ");
+    }
+}
