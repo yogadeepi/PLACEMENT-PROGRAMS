e61c38655a75ae7bf1aecc0a146c02b0b4024084
yogadeepi <yogadeepika13122004@gmail.com>
2025-07-30 21:25:36 +0530

    QuotientWithout.java

---
+++ QuotientWithout.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int count=0;
+        while(a>=b){
+            a=a-b;
+            count++;
+        }
+        System.out.print(count);
+    }
+}
