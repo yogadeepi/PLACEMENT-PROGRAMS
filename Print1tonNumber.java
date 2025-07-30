f29210ba253bddd98d8ba37e5454c058c6362ec9
yogadeepi <yogadeepika13122004@gmail.com>
2025-07-30 21:18:19 +0530

    Print1tonNumber.java

---
+++ Print1tonNumber.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        for(int i=1;i<=n;i++)
+            System.out.print(i+" ");
+    }
+}
