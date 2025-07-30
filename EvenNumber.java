0f86ca2d1a2f9db94b021d4b98bef092d1819bb1
yogadeepi <yogadeepika13122004@gmail.com>
2025-07-30 21:16:37 +0530

    EvenNumber.java

---
+++ EvenNumber.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        for(int i=a;i<=b;i++)
+            if(i%2==0)
+                System.out.print(i+" ");
+    }
+}
