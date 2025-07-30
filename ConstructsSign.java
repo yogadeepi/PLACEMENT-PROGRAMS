6c2bccd3e2b1b248b12431254c48f9474105af44
yogadeepi <yogadeepika13122004@gmail.com>
2025-07-30 21:52:10 +0530

    ConstructsSign.java

---
+++ ConstructsSign.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        if(n>0){
+            System.out.println("Positive");
+        }
+        else if(n<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Zero");
+        }
+    }
+}
