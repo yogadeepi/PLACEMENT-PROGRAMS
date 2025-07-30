4a89555dd66abf8e8ac9f621d4e8364de74d5d08
yogadeepi <yogadeepika13122004@gmail.com>
2025-07-30 21:47:46 +0530

    PIvalue.java

---
+++ PIvalue.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        double PI=22.0 / 7.0;
+        System.out.printf("%.12f\n",PI);
+    }
+}
