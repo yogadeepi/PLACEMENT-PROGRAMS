a9f3339d79c6b7a9550252f71faa4b81f48525cb
yogadeepi <yogadeepika13122004@gmail.com>
2025-07-30 21:38:39 +0530

    AcceptCharacter.java

---
+++ AcceptCharacter.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        System.out.println(ch);
+    }
+}
