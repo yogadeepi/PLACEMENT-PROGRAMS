fa37b0e320334ed3911000ceff2af4e0f1787c2c
yogadeepi <yogadeepika13122004@gmail.com>
2025-08-05 15:22:14 +0530

    GCD.java

---
+++ GCD.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int gcd=1;
+        for (int i = 1; i <= Math.min(a, b); i++) {
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
+        }
+
+        System.out.println("GCD of two number is " + gcd);
+        
+    }
+}
