9dda64761c0163ac8a686104c033359a43876792
yogadeepi <yogadeepika13122004@gmail.com>
2025-08-07 23:05:51 +0530

    plus sign.java

---
+++ plus sign.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int m1,m2,r,c;
+        if(n%2==0){
+            m1=n/2-1;
+            m2=n/2;
+        }else{
+            m1=m2=n/2;
+        }
+        for(r=0;r<n;r++){
+            for(c=0;c<n;c++){
+                if(r==m1 || r==m2 || c==m1 || c==m2){
+                    System.out.print("0");
+                }else{
+                    System.out.print("1");
+                }
+            }System.out.println();
+        }
+    }
+}
